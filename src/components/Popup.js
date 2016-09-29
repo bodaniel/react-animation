7b0b36e926111212bee67b8418da1615d8707096
Daniel Larsson <bodaniel@gmail.com>
2016-09-29 09:26:26 +0200

    Popup is closed when pressing close/hide button

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -6,9 +6,9 @@ const Popup = ({ visible, onClose }) => {
 	return (
 		<ReactCSSTransitionGroup transitionName="popup" transitionEnterTimeout={300} transitionLeaveTimeout={300}>
 		{visible && (
-			<div id="popup" onClick={onClose}>
+			<div id="popup">
 				<p>This is a popup popping up</p>
-				<button onClick={onClose}>Close</button>
+				<button onClick={onClose}>Hide</button>
 			</div>
 		)}
 		</ReactCSSTransitionGroup>
